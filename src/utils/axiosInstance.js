56ce0b3e8ea873e5fbff15ed381113387b05c57a
Abhishek Paul <jabhishekpaul079@gmail.com>
2025-07-23 14:01:20 +0530

    added new cors

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,20 +2,17 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
     baseURL:"https://url-shortener-backend-nqi0.onrender.com",
-    timeout:10000, //10s
+    timeout:5000,
     withCredentials:true
 })
 
-// Response interceptor
+
 axiosInstance.interceptors.response.use(
     (response) => {
-        // Any status code within the range of 2xx
         return response;
     },
     (error) => {
-        // Handle different types of errors
         if (error.response) {
-            // The server responded with a status code outside the 2xx range
             const { status, data } = error.response;
             
             switch (status) {
